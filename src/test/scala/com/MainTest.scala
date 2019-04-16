357417f28f3a0f14897a8e39ed09e177f878c7e4
Hryhorii Prokopiv <hryhorii@local.local>
2019-04-16 15:58:51 +0300

    removed garbadge

--- src/test/scala/com/MainTest.scala
+++ src/test/scala/com/MainTest.scala
@@ -2,7 +2,7 @@ package com
 
 import org.scalatest.{FreeSpec, Matchers}
 
-class SomeTest extends FreeSpec with Matchers{
+class MainTest extends FreeSpec with Matchers{
   "first test should add two numbers correctly" in {
 //    Calculator.add(1, 1) shouldEqual 2
   }
